f13d03a6735c16af9775d5c29af9a95a0f88c36a
almanfarroz <alman.farroz@gmail.com>
2025-06-09 16:46:12 +0700

    test

--- src/utils/cropImage.ts
+++ src/utils/cropImage.ts
@@ -4,15 +4,14 @@ const getCroppedImg = async (image: string, crop: Area): Promise<string> => {
   return new Promise((resolve, reject) => {
     const canvas = document.createElement('canvas');
     const context = canvas.getContext('2d');
-
+    canvas.width = crop.width;
+    canvas.height = crop.height;
     if (!context) throw new Error('2D context tidak tersedia');
 
     const imageSrc = new Image();
     imageSrc.src = image;
     
     imageSrc.onload = function (){
-      canvas.width = crop.width;
-      canvas.height = crop.height;
       context.drawImage(
         imageSrc, 
         crop.x,
